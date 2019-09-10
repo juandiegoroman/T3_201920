baac7b188f9aeb04b3094e89bf7ee692a737028d
Andres Bayona <al.bayona@uniandes.edu.co>
2019-09-10 16:02:38 -0500

    Changes

--- src/main/MVC.java
+++ src/main/MVC.java
@@ -1,10 +1,12 @@
 package main;
 import controller.Controller;
 
+
 public class MVC {
 	
 	public static void main(String[] args) 
 	{
+
 		Controller controler = new Controller();
 		controler.run();
 	}
